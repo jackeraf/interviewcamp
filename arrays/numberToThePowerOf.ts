603dc2292f686016c7862138e143038198a6ddd0
jackeraf <jarekimar@yahoo.es>
2020-04-01 17:01:39 +0200

    power to n

---
+++ arrays/numberToThePowerOf.ts
@@ -0,0 +1,28 @@
+/*
+ Implement a function to calculate X^N. Both X and N can be positiveor negative.
+*/
+
+// Time Complexity: O(log(N))
+// Space Complexity: O(log(N)) on call stack
+
+function powerToN(x: number, power: number) {
+  if (x == 0 && power <= 0) {
+    throw "undefined";
+  }
+  let result = positivePower(Math.abs(x), Math.abs(power));
+  if (power < 0) result = 1 / result;
+  if (x < 0 && power % 2 != 0) result = -1 * result;
+  return result;
+}
+function positivePower(x: number, power: number) {
+  if (power == 0) return 1;
+  if (power == 1) return x;
+  let halfPower = positivePower(x, Math.floor(power / 2));
+  if (power % 2 == 0) {
+    return halfPower * halfPower;
+  } else {
+    return x * halfPower * halfPower;
+  }
+}
+
+console.log(powerToN(5, 10));
